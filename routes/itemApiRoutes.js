01e0b7db2310e321797cd71abbe52dc83ce5f279
Ehren Davis <ehren.davis12@gmail.com>
2018-11-16 18:32:59 -0700

    This is the first pass at the item modle

---
+++ routes/itemApiRoutes.js
@@ -0,0 +1,43 @@
+var db = require("../models");
+
+module.exports = function(app) {
+
+  app.get("/api/v1/items", function(req, res) {
+    db.Item.findAll({}).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.get("/api/v1/items/:uuid", function(req, res) {
+    db.Item.findAll({
+      where:{
+        uuid: req.params.uuid
+      }
+    }).then(function(dbItem){
+      res.json(dbItem);
+    });
+  });
+
+  app.post("/api/v1/items", function(req, res) {
+    db.Item.create(req.body).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.put("/api/v1/items/", function(req, res) {
+    db.Item.update(
+      req.body,{
+        where:{
+          uuid: req.body.uuid
+        }
+      }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+
+  app.delete("/api/v1/items/:uuid", function(req, res) {
+    db.Item.destroy({ where: { uuid: req.params.uuid } }).then(function(dbItem) {
+      res.json(dbItem);
+    });
+  });
+};
