3d510ba9ca315b6809bdf6c94b7ed11ce91ed5a0
Ehren Davis <ehren.davis12@gmail.com>
2018-11-20 20:57:59 -0700

    Update Items model

--- models/item.js
+++ models/item.js
@@ -1,68 +1,73 @@
-module.exports = function(sequelize, DataTypes) {
-  
-  var Item = sequelize.define("Item", {
-	  uuid: {
-		  type: DataTypes.UUID,
-		  defaultValue: DataTypes.UUIDV1,
-		  primaryKey: true,
-		  isUUID: 4,
-	  },
-	  groupID: {
-		  type: DataTypes.STRING,
-		  allowNull: false,
-		  validate: {
-			  len: {
-				  args: [1, 255],
-				  msg: "groupID length is not in range of 1-255"
-			  }
-		  },
-		  defaultValue: false
-	  },
-	  name: {
-		  type: DataTypes.STRING,
-		  allowNull: false,
-		  validate: {
-			  len: {
-				  args: [1, 255],
-				  msg: "Name length is not in range of 1-255"
-			  }
-		  },
-		  defaultValue: false
-	  },
-	  description: {
-		  type: DataTypes.TEXT,
-		  allowNull: true,
-		  defaultValue: false
-	  },
-	  category: {
-		  type: DataTypes.STRING,
-		  allowNull: false,
-		  validate: {
-			  len: {
-				  args: [1, 255],
-				  msg: "category length is not in range of 1-255"
-			  }
-		  },
-		  defaultValue: "other"
-	  },
-	  quantity: {
-		  type: DataTypes.INTEGER					 ,
-		  allowNull: false,
-		  defaultValue: 0
-	  },
-	  expirationDate: {
-		  type: DataTypes.DATE,
-		  allowNull: true,
-		  isDate: true
-	  },
-	  waiteTimeOnOrderHours: {
-		  type: DataTypes.INTEGER,
-		  allowNull: true
-	  }
+module.exports = function (sequelize, DataTypes) {
 
-  },{
-	  freezeTableName: true
-  });
-  return Item;
+	var Item = sequelize.define("Item", {
+		uuid: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV1,
+			primaryKey: true,
+			isUUID: 4
+		},
+		sku: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			validate: {
+				len: {
+					args: [1, 255],
+					msg: "sku length is not in range of 1-255"
+				}
+			},
+			defaultValue: null
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 255],
+					msg: "Name length is not in range of 1-255"
+				}
+			},
+			defaultValue: false
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			defaultValue: null
+		},
+		category: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 255],
+					msg: "category length is not in range of 1-255"
+				}
+			},
+			defaultValue: "other"
+		},
+		quantity: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 0
+		},
+		expectedQuantity: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 0
+		},
+		expirationDate: {
+			type: DataTypes.DATE,
+			allowNull: true,
+			isDate: true
+		},
+		waiteTimeOnOrderHours: {
+			type: DataTypes.INTEGER,
+			allowNull: true
+		}
+
+	}, {
+			freezeTableName: true
+		});
+
+	return Item;
 };
-  
